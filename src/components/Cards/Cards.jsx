f4eb914a431479339072181c78fc22b873d1b78e
Alihassan001 <ali.official001@gmail.com>
2020-06-11 11:43:58 +0500

    render map on cards components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,39 +9,46 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
     if(!confirmed) {
         return 'Loading...'
     }
+
+    const data = [confirmed, recovered, deaths]
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                  <CardContent>
-                      <Typography color='textSecondary' gutterBottom>Infected</Typography>
-                      <Typography variant='h5'>
-                        <CountUp start={0} end={confirmed.value} duration={2.5} separator=',' />
-                      </Typography>
-                      <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
-                      <Typography variant='body2'>Number of active cases of covid-19</Typography>
-                  </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                  <CardContent>
-                      <Typography color='textSecondary' gutterBottom>Recovered</Typography>
-                      <Typography variant='h5'>
-                        <CountUp start={0} end={recovered.value} duration={2.5} separator=',' />
-                      </Typography>
-                      <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
-                      <Typography variant='body2'>Number of recoveries from covid-19</Typography>
-                  </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                  <CardContent>
-                      <Typography color='textSecondary' gutterBottom>Deaths</Typography>
-                      <Typography variant='h5'>
-                        <CountUp start={0} end={deaths.value} duration={2.5} separator=',' />
-                      </Typography>
-                      <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
-                      <Typography variant='body2'>Number of deaths caused by covid-19</Typography>
-                  </CardContent>
-                </Grid>
+              {data.map((val, ind) => {
+                return <Grid item component={Card} xs={12} md={3} className={
+                  val === confirmed ? cx(styles.card, styles.infected) :
+                  val === recovered ? cx(styles.card, styles.recovered) :
+                  val === deaths && cx(styles.card, styles.deaths)
+                  }>
+                <CardContent>
+                    <Typography color='textSecondary' gutterBottom>
+                      {
+                        val.confirmed ? 'Infected' :
+                        val.recovered ? 'Recovered' :
+                        val.deaths && 'Deaths'
+                      }
+                      
+                    </Typography>
+                    <Typography variant='h5'>
+                      <CountUp start={0} end={
+                        val === confirmed ? confirmed.value :
+                        val === recovered ? recovered.value :
+                        val === deaths && deaths.value
+                        } 
+                        duration={2.5} separator=',' />
+                    </Typography>
+                    <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+                    <Typography variant='body2'>
+                      {
+                        val === confirmed ? 'Number of active cases of covid-19' :
+                        val === recovered ? 'Number of recoveries from covid-19' :
+                        val === deaths && 'Number of deaths caused by covid-19'
+                      }
+                      
+                    </Typography>
+                </CardContent>
+              </Grid>
+              })}
             </Grid>
         </div>
     )
